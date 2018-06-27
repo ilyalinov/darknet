58dc137ee568cee1057edf72ace88a2e8a0edb08
Ilya Lyalinov <ilya.lyalinov@gmail.com>
2018-06-27 17:01:23 +0300

    detection with YOLO for webcam

--- src/yolo_with_tracking.cpp
+++ src/yolo_with_tracking.cpp
@@ -1,56 +1,82 @@
-#include <opencv2/core/utility.hpp>
-#include <opencv2/tracking.hpp>
-#include <opencv2/videoio.hpp>
+#include <string>
+#include <vector>
+#include <fstream>
+
+#define OPENCV
+#define GPU
+
+// import functions from YOLO DLL
+#include "yolo_v2_class.hpp"
+
+// opencv
+#include <opencv2/opencv.hpp>
 #include <opencv2/highgui.hpp>
-#include <iostream>
-#include <cstring>
 
 using namespace std;
 using namespace cv;
 
-int main(int argc, char** argv) {
-	// show help
-	if (argc<2) {
-		cout <<
-			" Usage: tracker <video_name>\n"
-			" examples:\n"
-			" example_tracking_kcf Bolt/img/%04d.jpg\n"
-			" example_tracking_kcf faceocc2.webm\n"
-			<< endl;
-		return 0;
+void draw_boxes(Mat mat_img, vector<bbox_t> result_vec, std::vector<std::string> obj_names) 
+{
+	for (auto &i : result_vec)
+	{
+		Scalar color(0, 0, 255);
+		rectangle(mat_img, Rect(i.x, i.y, i.w, i.h), color, 10);
+		if (obj_names.size() > i.obj_id)
+		{
+			putText(mat_img, obj_names[i.obj_id], Point2f(i.x, i.y - 10), FONT_HERSHEY_COMPLEX_SMALL, 1, color);
+		}
 	}
-	// declares all required variables
-	Rect2d roi;
-	Mat frame;
-	// create a tracker object
-	Ptr<Tracker> tracker = TrackerKCF::create();
-	// set input video
-	std::string video = argv[1];
-	VideoCapture cap(video);
-	// get bounding box
-	cap >> frame;
-	roi = selectROI("tracker", frame);
-	//quit if ROI was not selected
-	if (roi.width == 0 || roi.height == 0)
-		return 0;
-	// initialize the tracker
-	tracker->init(frame, roi);
-	// perform the tracking process
-	printf("Start the tracking process, press ESC to quit.\n");
-	for (;; ) {
-		// get frame from the video
+
+	imshow("window name", mat_img);
+}
+
+vector<string> obj_names_from_file(string const filename)
+{
+	ifstream f(filename);
+	vector<string> file_lines;
+	if (!f.is_open())
+	{
+		return file_lines;
+	}
+
+	string line;
+	while (getline(f, line))
+	{
+		file_lines.push_back(line);
+	}
+
+	f.close();
+	return file_lines;
+}
+
+int main()
+{
+	string names_file = "data/coco.names";
+	string weights_file = "yolov3.weights";
+	string cfg_file = "yolov3.cfg";
+
+	Detector detector(cfg_file, weights_file);
+	VideoCapture cap(0);
+	auto obj_names = obj_names_from_file(names_file);
+
+	// check if we succeeded
+	if (!cap.isOpened())
+	{
+		return -1;
+	}
+
+	while (true)
+	{
+		Mat frame;
 		cap >> frame;
-		// stop the program if no more images
-		if (frame.rows == 0 || frame.cols == 0)
+		auto result_vec = detector.detect(frame);
+		draw_boxes(frame, result_vec, obj_names);
+
+		if (waitKey(30) >= 0)
+		{
 			break;
-		// update the tracking result
-		tracker->update(frame, roi);
-		// draw the tracked object
-		rectangle(frame, roi, Scalar(255, 0, 0), 2, 1);
-		// show image with the tracked object
-		imshow("tracker", frame);
-		//quit on ESC button
-		if (waitKey(1) == 27)break;
+		}
 	}
+
 	return 0;
 }
